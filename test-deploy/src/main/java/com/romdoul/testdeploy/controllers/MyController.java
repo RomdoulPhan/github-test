5b80583e87635a150076e983f7aa92dc8a3fb34e
Romdoul <phan.romdoul2018@gmail.com>
2022-07-11 16:16:04 +0700

    add

---
+++ test-deploy/src/main/java/com/romdoul/testdeploy/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.romdoul.testdeploy.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/home")
+    public String home(){
+        return "Hello world";
+    }
+
+}
